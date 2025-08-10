93c59aa3f167708a4d23d187b5f5dd4d0581048e
Assel <asselaltayeva@users.noreply.github.com>
2025-08-10 17:15:19 +0900

    dashboard layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,7 +4,11 @@ import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 import { Providers } from "~/components/providers";
 import { Toaster } from "~/components/ui/sonner";
-
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
+import { AppSidebar } from "~/components/sidebar/app-sidebar";
+import { Separator } from "~/components/ui/separator";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "~/components/ui/breadcrumb";
+import BreadcrumbPageClient from "~/components/sidebar/breadcrumb-page-client";
 
 export const metadata: Metadata = {
   title: "Music Generator",
@@ -23,7 +27,29 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geist.variable}`}>
       <body className="flex min-h-svh flex-col">
-        <Providers>{children}
+        <Providers>
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset className="flex h-screen flex-col">
+              <header className="bg-background sticky-top z-10 border-b px-4 py-2">
+                <div className="flex shrink-0 grow items-center gap-2">
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator
+                    orientation="vertical"
+                    className="mr-2 data-[orientation=vertical]:h-4"
+                  /> 
+                  <Breadcrumb>
+                    <BreadcrumbList>
+                      <BreadcrumbItem>
+                        <BreadcrumbPageClient />
+                      </BreadcrumbItem>
+                    </BreadcrumbList>
+                  </Breadcrumb>
+                </div>
+              </header>
+          <main className="overflow-y-auto flex-1">{children}</main>
+          </SidebarInset>
+          </SidebarProvider>
           <Toaster />
         </Providers>
       </body>
